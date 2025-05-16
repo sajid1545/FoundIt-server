b41f756cf68c46255bdda63015b6a92a35beb49a
Sajjad Abdullah <clashking1545@gmail.com>
2025-05-16 23:18:44 +0600

    Filters lost items based on 'itemFound' status
    Ensures that only lost items (where 'itemFound' is false) are retrieved in the getLostItems query. This prevents already found items from appearing in the list of lost items.

--- src/app/modules/LostItem/lostItem.service.ts
+++ src/app/modules/LostItem/lostItem.service.ts
@@ -3,261 +3,277 @@ import { Request } from "express";
 import { paginationHelper } from "../../../helpers/paginationHelper";
 import prisma from "../../../shared/prisma";
 import { TPaginationOptions } from "../../interfaces/pagination";
-import { filterableSortBy, lostItemSearchAbleFields } from "./lostItem.constants";
+import {
+  filterableSortBy,
+  lostItemSearchAbleFields,
+} from "./lostItem.constants";
 import { TLostItemsFilterRequest } from "./lostItem.interface";
 
 const createLostItem = async (req: Request & { user?: any }) => {
-	const user = req.user;
+  const user = req.user;
 
-	const payload = req.body;
+  const payload = req.body;
 
-	// const file = req.file as IFile;
+  // const file = req.file as IFile;
 
-	// if (file) {
-	// 	const uploadToCloudinary = await imageUploader.uploadToCloudinary(file);
-	// 	req.body.image = uploadToCloudinary?.secure_url;
-	// }
+  // if (file) {
+  // 	const uploadToCloudinary = await imageUploader.uploadToCloudinary(file);
+  // 	req.body.image = uploadToCloudinary?.secure_url;
+  // }
 
-	const result = await prisma.$transaction(async (tsx) => {
-		const userData = await tsx.user.findUniqueOrThrow({
-			where: {
-				id: user.id,
-			},
-			select: {
-				id: true,
-				name: true,
-				email: true,
-				password: false,
-				createdAt: true,
-				updatedAt: true,
-			},
-		});
+  const result = await prisma.$transaction(async (tsx) => {
+    const userData = await tsx.user.findUniqueOrThrow({
+      where: {
+        id: user.id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: false,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
 
-		const categoryData = await tsx.foundItemCategory.findUniqueOrThrow({
-			where: {
-				id: payload.categoryId,
-			},
-		});
+    const categoryData = await tsx.foundItemCategory.findUniqueOrThrow({
+      where: {
+        id: payload.categoryId,
+      },
+    });
 
-		const createLostItem = await tsx.lostItem.create({
-			data: {
-				...payload,
-				name: userData.name,
-				email: user.email,
-				userId: user.id,
-			},
-		});
+    const createLostItem = await tsx.lostItem.create({
+      data: {
+        ...payload,
+        name: userData.name,
+        email: user.email,
+        userId: user.id,
+      },
+    });
 
-		const responseObject = {
-			...createLostItem,
-			user: userData,
-			category: categoryData,
-		};
+    const responseObject = {
+      ...createLostItem,
+      user: userData,
+      category: categoryData,
+    };
 
-		return responseObject;
-	});
+    return responseObject;
+  });
 
-	return result;
+  return result;
 };
 
-const getLostItems = async (params: TLostItemsFilterRequest, options: TPaginationOptions) => {
-	const { page, limit, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
+const getLostItems = async (
+  params: TLostItemsFilterRequest,
+  options: TPaginationOptions
+) => {
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelper.calculatePagination(options);
 
-	let sort;
+  let sort;
 
-	// filterableSortBy: ["foundItemName", "category"] -- sortby can be done only in these fields
-	if (filterableSortBy.map((field) => field === sortBy).includes(true)) {
-		sort = sortBy;
-	} else {
-		sort = "createdAt";
-	}
+  // filterableSortBy: ["foundItemName", "category"] -- sortby can be done only in these fields
+  if (filterableSortBy.map((field) => field === sortBy).includes(true)) {
+    sort = sortBy;
+  } else {
+    sort = "createdAt";
+  }
 
-	const andConditions: Prisma.LostItemWhereInput[] = [];
+  const andConditions: Prisma.LostItemWhereInput[] = [];
 
-	const { searchTerm, ...itemsFilter } = params;
+  const { searchTerm, ...itemsFilter } = params;
 
-	if (params.searchTerm) {
-		andConditions.push({
-			OR: lostItemSearchAbleFields.map((field) => ({
-				[field]: {
-					contains: params.searchTerm,
-					mode: "insensitive",
-				},
-			})),
-		});
-	}
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: lostItemSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
 
-	// filter by category
+  // filter by category
 
-	let categoryFilter = "";
+  let categoryFilter = "";
 
-	if ((itemsFilter as any).category) {
-		categoryFilter = (itemsFilter as any).category;
-	}
+  if ((itemsFilter as any).category) {
+    categoryFilter = (itemsFilter as any).category;
+  }
 
-	const ss = Object.keys(itemsFilter);
+  const ss = Object.keys(itemsFilter);
 
-	if (ss.length > 0 && ss.includes("category")) {
-		andConditions.push({
-			category: {
-				name: {
-					contains: categoryFilter,
-					mode: "insensitive",
-				},
-			},
-		});
-	}
+  if (ss.length > 0 && ss.includes("category")) {
+    andConditions.push({
+      category: {
+        name: {
+          contains: categoryFilter,
+          mode: "insensitive",
+        },
+      },
+    });
+  }
 
-	delete (itemsFilter as any).category;
+  delete (itemsFilter as any).category;
 
-	if (Object.keys(itemsFilter).length > 0) {
-		andConditions.push({
-			AND: Object.keys(itemsFilter).map((key) => ({
-				[key]: {
-					equals: (itemsFilter as any)[key],
-				},
-			})),
-		});
-	}
+  if (Object.keys(itemsFilter).length > 0) {
+    andConditions.push({
+      AND: Object.keys(itemsFilter).map((key) => ({
+        [key]: {
+          equals: (itemsFilter as any)[key],
+        },
+      })),
+    });
+  }
 
-	const whereConditions: Prisma.LostItemWhereInput =
-		andConditions.length > 0 ? { AND: andConditions } : {};
+  const whereConditions: Prisma.LostItemWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
 
-	const result = await prisma.lostItem.findMany({
-		where: whereConditions,
-		include: {
-			user: {
-				select: {
-					id: true,
-					name: true,
-					email: true,
-					password: false,
-					createdAt: true,
-					updatedAt: true,
-				},
-			},
-			category: true,
-		},
-		skip,
-		take: limit,
-		orderBy: sort === "category" ? { category: { name: sortOrder as any } } : { [sort]: sortOrder },
-	});
+  const result = await prisma.lostItem.findMany({
+    where: {
+      itemFound: false,
+      ...whereConditions,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          password: false,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+      category: true,
+    },
+    skip,
+    take: limit,
+    orderBy:
+      sort === "category"
+        ? { category: { name: sortOrder as any } }
+        : { [sort]: sortOrder },
+  });
 
-	const total = await prisma.lostItem.count({
-		where: whereConditions,
-	});
+  const total = await prisma.lostItem.count({
+    where: {
+      itemFound: false,
+      ...whereConditions,
+    },
+  });
 
-	return {
-		meta: {
-			page,
-			limit,
-			total,
-		},
-		data: result,
-	};
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
 };
 
 const updateLostItem = async (id: string, payload: any) => {
-	const isLostItemExists = await prisma.lostItem.findUniqueOrThrow({
-		where: {
-			id,
-		},
-	});
+  const isLostItemExists = await prisma.lostItem.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
 
-	const result = await prisma.lostItem.update({
-		where: {
-			id,
-		},
-		data: {
-			...payload,
-		},
-	});
-	return result;
+  const result = await prisma.lostItem.update({
+    where: {
+      id,
+    },
+    data: {
+      ...payload,
+    },
+  });
+  return result;
 };
 
 const deleteLostItem = async (id: string) => {
-	const isLostItemExists = await prisma.lostItem.findUniqueOrThrow({
-		where: {
-			id,
-		},
-	});
+  const isLostItemExists = await prisma.lostItem.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
 
-	const result = await prisma.lostItem.delete({
-		where: {
-			id,
-		},
-	});
-	return result;
+  const result = await prisma.lostItem.delete({
+    where: {
+      id,
+    },
+  });
+  return result;
 };
 
 const changeLostItemFoundStatus = async (id: string) => {
-	const isLostItemExists = await prisma.lostItem.findUniqueOrThrow({
-		where: {
-			id,
-		},
-	});
+  const isLostItemExists = await prisma.lostItem.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
 
-	const result = await prisma.lostItem.update({
-		where: {
-			id,
-		},
-		data: {
-			itemFound: !isLostItemExists.itemFound,
-		},
-	});
-	return result;
+  const result = await prisma.lostItem.update({
+    where: {
+      id,
+    },
+    data: {
+      itemFound: !isLostItemExists.itemFound,
+    },
+  });
+  return result;
 };
 
 const myLostItems = async (user: any) => {
-	const result = await prisma.lostItem.findMany({
-		where: {
-			userId: user.id,
-		},
-		include: {
-			user: {
-				select: {
-					id: true,
-					name: true,
-					email: true,
-					password: false,
-					createdAt: true,
-					updatedAt: true,
-				},
-			},
-			category: true,
-		},
-	});
-	return result;
+  const result = await prisma.lostItem.findMany({
+    where: {
+      userId: user.id,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          password: false,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+      category: true,
+    },
+  });
+  return result;
 };
 
 const getSingleLostItem = async (id: string) => {
-	const result = await prisma.lostItem.findUniqueOrThrow({
-		where: {
-			id,
-		},
-		include: {
-			user: {
-				select: {
-					id: true,
-					name: true,
-					email: true,
-					password: false,
-					createdAt: true,
-					updatedAt: true,
-				},
-			},
-			category: true,
-		},
-	});
-	return result;
+  const result = await prisma.lostItem.findUniqueOrThrow({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          password: false,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+      category: true,
+    },
+  });
+  return result;
 };
 
 export const LostItemServices = {
-	createLostItem,
-	getLostItems,
-	updateLostItem,
-	deleteLostItem,
-	changeLostItemFoundStatus,
-	myLostItems,
-	getSingleLostItem,
+  createLostItem,
+  getLostItems,
+  updateLostItem,
+  deleteLostItem,
+  changeLostItemFoundStatus,
+  myLostItems,
+  getSingleLostItem,
 };
